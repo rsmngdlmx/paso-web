91eb62132f4becf638e8915a8b3bbffde2d080f9
Ricardo Mendoza <rsmn.development@gmail.com>
2026-08-25 00:56:22 -0600

    Prepare for production

--- src/pasoweb/settings.py
+++ src/pasoweb/settings.py
@@ -1,17 +1,11 @@
 from pathlib import Path
+import os
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lo+4#axw7f(gmy$bpeu)f2&+!4-0uszfyck^%8ioj8=1k5-==a'
-
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
-# Application definition
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'lo+4#axw7f(gmy$bpeu)f2&+!4-0uszuck^%8ioj8=1k5-==ace79$fs3c44a[44961qey0d2wd99p//R10h')
+DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = [os.environ.get('WEBSITE_HOSTNAME', 'localhost')]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -25,6 +19,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -100,8 +95,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
-
-# Default primary key field type
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
